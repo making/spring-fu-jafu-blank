055afa68e2894247e048feb4b758acdf9f96ccc8
Toshiaki Maki <tmaki@pivotal.io>
2018-10-29 15:52:46 +0900

    Switch to Integration Test

--- src/test/java/xxxxxx/yyyyyy/zzzzzz/HelloHandlerTest.java
+++ src/test/java/xxxxxx/yyyyyy/zzzzzz/HelloHandlerTest.java
@@ -1,15 +1,19 @@
 package xxxxxx.yyyyyy.zzzzzz;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 public class HelloHandlerTest {
+    private ConfigurableApplicationContext context;
     private WebTestClient testClient;
 
     @Before
     public void setUp() throws Exception {
-        this.testClient = WebTestClient.bindToRouterFunction(new HelloHandler().routes()).build();
+        this.context = App.app.run();
+        this.testClient = WebTestClient.bindToServer().baseUrl("http://localhost:8080").build();
     }
 
     @Test
@@ -19,5 +23,10 @@ public class HelloHandlerTest {
                 .exchange() //
                 .expectStatus().isOk() //
                 .expectBody(String.class).isEqualTo("Hello World!");
+    }
+
+    @After
+    public void tearDown() {
+        this.context.close();
     }
 }
--- src/test/java/xxxxxx/yyyyyy/zzzzzz/MessageHandlerTest.java
+++ src/test/java/xxxxxx/yyyyyy/zzzzzz/MessageHandlerTest.java
@@ -1,18 +1,22 @@
 package xxxxxx.yyyyyy.zzzzzz;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import static org.hamcrest.Matchers.is;
 
 public class MessageHandlerTest {
+    private ConfigurableApplicationContext context;
     private WebTestClient testClient;
 
     @Before
     public void setUp() throws Exception {
-        this.testClient = WebTestClient.bindToRouterFunction(new MessageHandler().routes()).build();
+        this.context = App.app.run();
+        this.testClient = WebTestClient.bindToServer().baseUrl("http://localhost:8080").build();
     }
 
     @Test
@@ -42,5 +46,10 @@ public class MessageHandlerTest {
                 .expectBody(JsonNode.class)
                 .value(n -> n.get(0).get("defaultMessage").asText(),
                         is("The size of \"text\" must be less than or equal to 8. The given size is 10"));
+    }
+
+    @After
+    public void tearDown() {
+        this.context.close();
     }
 }
