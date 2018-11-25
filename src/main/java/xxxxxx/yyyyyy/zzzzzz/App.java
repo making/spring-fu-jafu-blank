8f9fbbda01ae57add18e368edadc85fdb1026a8d
Toshiaki Maki <tmaki@pivotal.io>
2018-11-25 21:29:44 +0900

    Follow latest updates

--- src/main/java/xxxxxx/yyyyyy/zzzzzz/App.java
+++ src/main/java/xxxxxx/yyyyyy/zzzzzz/App.java
@@ -1,37 +1,22 @@
 package xxxxxx.yyyyyy.zzzzzz;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.web.codec.CodecCustomizer;
-import org.springframework.fu.jafu.Jafu;
-import org.springframework.http.codec.ServerSentEventHttpMessageWriter;
-import org.springframework.http.codec.json.Jackson2JsonDecoder;
-import org.springframework.http.codec.json.Jackson2JsonEncoder;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.core.env.Profiles;
+import org.springframework.fu.jafu.ApplicationDsl;
 
 public class App {
-    static SpringApplication app = Jafu.application(app -> {
-        app.beans(beans -> {
-            beans.bean(HelloHandler.class);
-            beans.bean(MessageHandler.class);
-            beans.bean(ObjectMapper.class, () -> Jackson2ObjectMapperBuilder.json().build());
-            beans.bean(CodecCustomizer.class, () -> configurer -> {
-                // TODO should be provided as a part of DSL
-                ObjectMapper mapper = app.ref(ObjectMapper.class);
-                configurer.customCodecs().decoder(new Jackson2JsonDecoder(mapper));
-                Jackson2JsonEncoder encoder = new Jackson2JsonEncoder(mapper);
-                configurer.customCodecs().encoder(encoder);
-                configurer.customCodecs().writer(new ServerSentEventHttpMessageWriter(encoder));
-            });
-        });
-        app.server(server -> server.router(router -> {
-            router //
-                    .add(app.ref(HelloHandler.class).routes()) //
-                    .add(app.ref(MessageHandler.class).routes());
-        }));
-    });
 
-    public static void main(String[] args) {
-        app.run(args);
-    }
+	static ApplicationDsl app = ApplicationDsl.application(app -> app //
+			.beans(beans -> beans //
+					.bean(HelloHandler.class) //
+					.bean(MessageHandler.class))
+			.server(server -> server
+					.port(app.env().acceptsProfiles(Profiles.of("test")) ? 8181 : 8080) //
+					.router(router -> router //
+							.add(app.ref(HelloHandler.class).routes()) //
+							.add(app.ref(MessageHandler.class).routes())) //
+					.codecs(codecs -> codecs.string().jackson())));
+
+	public static void main(String[] args) {
+		app.run(args);
+	}
 }
